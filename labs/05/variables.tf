5468656c2db4a4e46a817fcb303f42dae5950148
qatip <michael2697@hotmail.com>
2025-08-06 13:21:27 +0100

    Update variables.tf

--- labs/05/variables.tf
+++ labs/05/variables.tf
@@ -5,7 +5,7 @@ variable "vm_size" {
     condition = contains([
       "Standard_D2s_V3",
       "Standard_DS2_V2",
-      "Standard_B2S",
+      "Standard_B2s",
       "Standard_DS3_v2"
     ], var.vm_size)
     error_message = "Only the following VM sizes are allowed in this lab: Standard_D2s_V3, Standard_DS2_V2, Standard_B2s, Standard_DS3_v2."
