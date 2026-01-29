04b6d7b0913c1b487ca0fdff6a3d81dc65c5d282
qatip <michael2697@hotmail.com>
2026-01-29 17:20:41 +0000

    Update setup_jenkins_azure.sh

--- labs/08/setup_jenkins_azure.sh
+++ labs/08/setup_jenkins_azure.sh
@@ -10,7 +10,7 @@ sudo apt-get install -y openjdk-17-jdk
 
 echo "Adding Jenkins repository..."
 sudo apt-get install -y curl gnupg lsb-release
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 
