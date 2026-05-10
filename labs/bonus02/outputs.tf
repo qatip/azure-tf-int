e433ed6aff4c5024a5b4e7be1af5339118c1e654
qatip <michael2697@hotmail.com>
2026-05-10 21:56:31 +0100

    Update outputs.tf

--- labs/bonus02/outputs.tf
+++ labs/bonus02/outputs.tf
@@ -1,5 +1,5 @@
 output "nginx_service_ip" {
-  value       = kubernetes_service.nginx.status[0].load_balancer[0].ingress[0].ip
+  value       = kubernetes_service_v1.nginx.status[0].load_balancer[0].ingress[0].ip
   description = "The public IP address of the NGINX service"
 }
 
