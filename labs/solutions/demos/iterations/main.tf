7182f21beeb3e378356400c25c63e20b8a3c0389
azureuser <azureuser@lab.local>
2026-06-08 10:08:57 +0100

    iteration demo update

--- labs/solutions/demos/iterations/main.tf
+++ labs/solutions/demos/iterations/main.tf
@@ -10,28 +10,18 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<sub id>"
+  subscription_id = "911e746f-0030-41d8-839c-c3579ec74ee4"
 }
 
+
+/*
 # Create Resource Groups dynamically using count
 resource "azurerm_resource_group" "example1" {
   count    = var.resource_group_count
-
-  name     = "rg-demo-${count.index + 1}"
+  name     = "rg-demo-${count.index}"
   location = var.location
 }
 
-
-
-# Create multiple Resource Groups using for_each
-resource "azurerm_resource_group" "example2" {
-  for_each = var.resource_groups
-  name     = each.key
-  location = each.value
-}
-
-#
-# Output: Example 1 - Resource group names and regions
 output "resource_groups_example_1" {
   description = "Resource group names with their locations"
   value = [
@@ -39,12 +29,47 @@ output "resource_groups_example_1" {
     "${rg.name} is in ${rg.location}"
   ]
 }
+*/
+
+
+/*
+# Resource groups created using for_each from lists with index-based keys"
+resource "azurerm_resource_group" "example2" {
+  for_each = {
+    for index, name in var.rg-names : index => {
+      name     = name
+      location = var.locations[index]
+    }
+  }
+  name     = each.value.name
+  location = each.value.location
+}
+
+output "resource_groups_example_2" {
+  description = "Resource groups created using for_each from lists with index-based keys"
+  value = {
+    for index_key, rg in azurerm_resource_group.example2 :
+    index_key => {
+      name     = rg.name
+      location = rg.location
+    }
+  }
+}
+*/
+
+#/*
+# Create multiple Resource Groups using for_each
+resource "azurerm_resource_group" "example3" {
+  for_each = var.resource_groups
+  name     = each.key
+  location = each.value
+}
 
 # Outputs
-output "resource_groups_example_2" {
+output "resource_groups_example_3" {
   description = "Resource groups created with their locations"
-  value = { for rg_name, rg in azurerm_resource_group.example2 : 
+  value = { for rg_name, rg in azurerm_resource_group.example3 :
     rg_name => rg.location
   }
 }
-
+#*/
--- labs/solutions/demos/iterations/variables.tf
+++ labs/solutions/demos/iterations/variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_count" {
   description = "The number of Resource Groups to create"
   type        = number
-  default     = 3
+  default     = 2
 }
 
 variable "location" {
@@ -10,12 +10,30 @@ variable "location" {
   default     = "East US"
 }
 
+variable "rg-names" {
+  description = "RG Names"
+  type        = list(string)
+  default     = ["rg-test", "rg-prod"]
+  # default = ["rg-dev","rg-test","rg-prod"]
+
+}
+
+variable "locations" {
+  description = "Locations"
+  type        = list(string)
+  default     = ["West Europe", "North Europe"]
+  #  default = ["East US","West Europe","North Europe"]
+}
+
+
+
 variable "resource_groups" {
   description = "Map of resource group names to their locations"
   type        = map(string)
   default = {
-    "rg-east" = "East US"
-    "rg-west" = "West Europe"
+    # "rg-east" = "East US"
+    "rg-west"    = "West Europe"
     "rg-central" = "Central US"
   }
 }
+
